2ffea71638994a52fccea31f4187dae56ac0d159
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2021-02-03 15:28:06 +0100

    Bump v0.3.1
    * Add new translations
    * Remove duplicated file for Norwegian Bokmål language
    * Update copyright & GitLab links
    * Add description & GitLab link
    * NPM audit fix

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class NetbootBuild(build_py):
 
 setuptools.setup(
     name=NAME,
-    version='0.3.0',
+    version='0.3.1',
     packages=setuptools.find_packages(exclude=['tests']),
     include_package_data=True,
 
